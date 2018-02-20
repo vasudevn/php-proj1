f8e69153719b912af675c2d29f6070bad857be9a
test1 <test1@mylabs.com>
2018-02-20 11:28:17 -0500

    final update

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "This is GIT Jenkins Integrate Web" ?>
+echo "This is GIT Jenkins Integrate Web completed successfully" ?>
--- test_hello.php
+++ test_hello.php
@@ -3,7 +3,7 @@ ob_start();
 require("index.php");
 $result = ob_get_clean();
 
-if( $result != 'This is GIT Jenkins Integrate Web' ) {
+if( $result != 'This is GIT Jenkins Integrate Web completed successfully' ) {
    exit(1);
 }
 exit(0);
